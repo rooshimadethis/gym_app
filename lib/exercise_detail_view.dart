c66e2e00c83ee4cfab1cc7d08c885c8cf0932f02
rooshimadethis <rooshipatidar@gmail.com>
2025-11-10 15:07:28 -0500

    perff. this is nuts

--- lib/exercise_detail_view.dart
+++ lib/exercise_detail_view.dart
@@ -167,153 +167,156 @@ class _ExerciseDetailViewState extends State<ExerciseDetailView> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            widget.exercise.hasLocalImage && widget.exercise.imageUrl != null
-                ? Image.asset(
-                    widget.exercise.imageUrl!,
-                    fit: BoxFit.cover,
-                  )
-                : CachedNetworkImage(
-                    imageUrl: placeHolderImageUrl,
-                    placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) => const Icon(Icons.error),
-                  ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                widget.exercise.name,
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ),
-            // The Expanded widget below needs to be removed or adjusted when wrapped in SingleChildScrollView
-            // as Expanded inside SingleChildScrollView doesn't make sense.
-            // Instead, we'll make the ListView.builder shrinkWrap and physics scrollable.
-            ListView.builder(
-              shrinkWrap: true, // Important: make ListView take only needed space
-              physics: const NeverScrollableScrollPhysics(), // Disable its own scrolling
-              itemCount: widget.exercise.sets.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 30,
-                        child: Text(
-                          '${index + 1}.',
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          controller: _weightControllers[index],
-                          focusNode: _weightFocusNodes[index],
-                          decoration: InputDecoration(
-                            labelText: 'Weight',
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
-                            ),
-                          ),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(3),
-                          ],
-                          onSubmitted: (_) {
-                            FocusScope.of(context).requestFocus(_repsFocusNodes[index]);
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text(
-                        'x',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      const SizedBox(width: 8.0),
-                      Expanded(
-                        child: TextField(
-                          controller: _repsControllers[index],
-                          focusNode: _repsFocusNodes[index],
-                          decoration: InputDecoration(
-                            labelText: 'Reps',
-                            border: OutlineInputBorder(),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
-                            ),
-                          ),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(2),
-                          ],
-                          onSubmitted: (_) {
-                            HapticFeedback.mediumImpact();
-                            _saveData(); // Save current data
-                            if (index == widget.exercise.sets.length - 1) {
-                              Navigator.pop(context); // Finish exercise
-                            } else {
-                              _logSet(); // Log set and move to next
-                            }
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  ElevatedButton.icon(
-                    onPressed: _removeSet,
-                    icon: const Icon(Icons.remove),
-                    label: const Text('Remove Set'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.secondary,
-                      foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                  widget.exercise.hasLocalImage && widget.exercise.imageUrl != null
+                      ? Image.asset(
+                          widget.exercise.imageUrl!,
+                          fit: BoxFit.cover,
+                        )
+                      : CachedNetworkImage(
+                          imageUrl: placeHolderImageUrl,
+                          placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                        ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      widget.exercise.name,
+                      style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
                     ),
                   ),
-                  const SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: _addSet,
-                    icon: const Icon(Icons.add),
-                    label: const Text('Add Set'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                    ),
+                  ListView.builder(
+                    shrinkWrap: true, // Important: make ListView take only needed space
+                    physics: const NeverScrollableScrollPhysics(), // Disable its own scrolling
+                    itemCount: widget.exercise.sets.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 30,
+                              child: Text(
+                                '${index + 1}.',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            ),
+                            Expanded(
+                              child: TextField(
+                                controller: _weightControllers[index],
+                                focusNode: _weightFocusNodes[index],
+                                decoration: InputDecoration(
+                                  labelText: 'Weight',
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                  LengthLimitingTextInputFormatter(3),
+                                ],
+                                onSubmitted: (_) {
+                                  FocusScope.of(context).requestFocus(_repsFocusNodes[index]);
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 8.0),
+                            Text(
+                              'x',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            const SizedBox(width: 8.0),
+                            Expanded(
+                              child: TextField(
+                                controller: _repsControllers[index],
+                                focusNode: _repsFocusNodes[index],
+                                decoration: InputDecoration(
+                                  labelText: 'Reps',
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                  LengthLimitingTextInputFormatter(2),
+                                ],
+                                onSubmitted: (_) {
+                                  HapticFeedback.mediumImpact();
+                                  _saveData(); // Save current data
+                                  if (index == widget.exercise.sets.length - 1) {
+                                    Navigator.pop(context); // Finish exercise
+                                  } else {
+                                    _logSet(); // Log set and move to next
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  HapticFeedback.mediumImpact();
-                  _saveData();
-                  if (isLastSetFocused) {
-                    Navigator.pop(context);
-                  } else {
-                    _logSet();
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                  minimumSize: const Size(double.infinity, 50),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: _removeSet,
+                  icon: const Icon(Icons.remove),
+                  label: const Text('Remove Set'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                  ),
                 ),
-                child: Text(isLastSetFocused ? 'Finish Exercise' : 'Log Set'),
-              ),
+                const SizedBox(width: 16),
+                ElevatedButton.icon(
+                  onPressed: _addSet,
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add Set'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                HapticFeedback.mediumImpact();
+                _saveData();
+                if (isLastSetFocused) {
+                  Navigator.pop(context);
+                } else {
+                  _logSet();
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: Text(isLastSetFocused ? 'Finish Exercise' : 'Log Set'),
+            ),
+          ),
+        ],
       ),
     );
   }
