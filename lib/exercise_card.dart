78e1ca58b03cb6c9c5d06e83db22a0b601358900
rooshimadethis <rooshipatidar@gmail.com>
2025-11-15 14:07:11 -0500

    decent animation

--- lib/exercise_card.dart
+++ lib/exercise_card.dart
@@ -27,26 +27,17 @@ class _ExerciseCardState extends State<ExerciseCard>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _scaleAnimation;
-  late Animation<Matrix4> _transformAnimation;
+  TickerFuture? _ticker;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 200),
+      duration: const Duration(milliseconds: 100),
     );
 
     _scaleAnimation = Tween<double>(begin: 1.0, end: 0.95).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
-
-    _transformAnimation = Matrix4Tween(
-      begin: Matrix4.identity(),
-      end: Matrix4.identity()
-        ..setEntry(3, 2, 0.001)
-        ..rotateY(-0.1),
-    ).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
   }
@@ -62,112 +53,105 @@ class _ExerciseCardState extends State<ExerciseCard>
     final placeHolderImageUrl =
         'https://placehold.co/400x200.png?text=${Uri.encodeComponent(widget.exercise.name)}';
 
-    return MouseRegion(
-      onEnter: (_) => _controller.forward(),
-      onExit: (_) => _controller.reverse(),
-      child: GestureDetector(
-        onTapDown: (_) => _controller.forward(),
-        onTapUp: (_) => _controller.reverse(),
-        onTapCancel: () => _controller.reverse(),
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform(
-              transform: _transformAnimation.value,
-              alignment: FractionalOffset.center,
-              child: ScaleTransition(
-                scale: _scaleAnimation,
-                child: child,
-              ),
-            );
-          },
-          child: Container(
-            decoration: BoxDecoration(
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha((255 * 0.3).round()),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: GestureDetector(
+          onLongPress: widget.onLongPress,
+          child: OpenContainer(
+            tappable: false,
+            closedColor: Theme.of(context).colorScheme.primary,
+            closedShape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withAlpha((255 * 0.3).round()),
-                  spreadRadius: 1,
-                  blurRadius: 10,
-                  offset: const Offset(0, 5),
-                ),
-              ],
             ),
-            child: GestureDetector(
-              onLongPress: widget.onLongPress,
-              child: OpenContainer(
-                tappable: false,
-                closedColor: Theme.of(context).colorScheme.primary,
-                closedShape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                closedElevation: 0.0,
-                transitionDuration: const Duration(milliseconds: 300),
-                openBuilder: (context, action) {
-                  return ExerciseDetailView(
-                    exercise: widget.exercise,
-                    onExerciseCompleted: widget.onExerciseCompleted,
-                  );
+            closedElevation: 0.0,
+            transitionDuration: const Duration(milliseconds: 300),
+            openBuilder: (context, action) {
+              return ExerciseDetailView(
+                exercise: widget.exercise,
+                onExerciseCompleted: widget.onExerciseCompleted,
+              );
+            },
+            closedBuilder: (context, action) {
+              return GestureDetector(
+                onTapDown: (_) {
+                  _ticker = _controller.forward();
                 },
-                closedBuilder: (context, action) {
-                  return GestureDetector(
-                    onTap: () {
-                      HapticFeedback.lightImpact();
-                      widget.onTap();
-                      action();
-                    },
-                    child: AspectRatio(
-                      aspectRatio: 2 / 1,
-                      child: Card(
-                        elevation: 0,
-                        color: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: widget.exercise.hasLocalImage &&
-                                      widget.exercise.imageUrl != null
-                                  ? Image.asset(
-                                      widget.exercise.imageUrl!,
-                                      fit: BoxFit.contain,
-                                      width: double.infinity,
-                                    )
-                                  : CachedNetworkImage(
-                                      imageUrl: placeHolderImageUrl,
-                                      fit: BoxFit.contain,
-                                      width: double.infinity,
-                                      placeholder: (context, url) =>
-                                          Container(color: Colors.grey[300]),
-                                      errorWidget: (context, url, error) =>
-                                          const Icon(Icons.error),
-                                    ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                widget.exercise.name,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headlineSmall
-                                    ?.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary,
-                                    ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                onTapUp: (_) {
+                  _ticker?.whenCompleteOrCancel(() {
+                    if (mounted) {
+                      _controller.reverse();
+                    }
+                  });
+                },
+                onTapCancel: () {
+                  _controller.reverse();
+                },
+                onTap: () {
+                  HapticFeedback.lightImpact();
+                  widget.onTap();
+                  Future.delayed(const Duration(milliseconds: 100), action);
+                },
+                child: AspectRatio(
+                  aspectRatio: 2 / 1,
+                  child: Card(
+                    elevation: 0,
+                    color: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
                     ),
-                  );
-                },
-              ),
-            ),
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: widget.exercise.hasLocalImage &&
+                                  widget.exercise.imageUrl != null
+                              ? Image.asset(
+                                  widget.exercise.imageUrl!,
+                                  fit: BoxFit.contain,
+                                  width: double.infinity,
+                                )
+                              : CachedNetworkImage(
+                                  imageUrl: placeHolderImageUrl,
+                                  fit: BoxFit.contain,
+                                  width: double.infinity,
+                                  placeholder: (context, url) =>
+                                      Container(color: Colors.grey[300]),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            widget.exercise.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall
+                                ?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
